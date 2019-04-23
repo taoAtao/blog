fc7c505d7aef9e8fcaf039182a5bb6fb5326c92f
taoAtao <xiangtao1994@hotmail.com>
2019-04-23 19:01:53 +0800

    springMVC+spring整合3

--- src/test/java/testMybatis.java
+++ src/test/java/testMybatis.java
@@ -3,7 +3,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import com.cnc.blog.pojo.UserInfo;
 import com.cnc.blog.service.UserInfoServiceImpl;
 
 /**
@@ -16,6 +15,8 @@ public class testMybatis {
     @Autowired
     private UserInfoServiceImpl userService;
 
+//    static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+
 
     @Test
     public void testList() throws Exception {
@@ -24,10 +25,18 @@ public class testMybatis {
 //
 //        UserInfo userInfo = userService.getUserInfoById(1);
 //        System.out.println(userInfo.toString());
-        UserInfo userInfo = new UserInfo();
-        userInfo.setName("xx");
-        userInfo.setPassword("222");
-        System.out.println(userService.checkUser("阿斯达斯的eqwqwe123123"));
+//        UserInfo userInfo = new UserInfo();
+//        userInfo.setName("xx");
+//        userInfo.setPassword("222");
+//        System.out.println(userService.checkUser("阿斯达斯的eqwqwe123123"));
+//        logger.trace("trace message");
+//        logger.debug("debug message");
+//        logger.info("info message");
+//        logger.warn("warn message");
+//        logger.error("error message");
+//        logger.fatal("fatal message");
+//        System.out.println("Hello World!");
+
     }
 
 }
